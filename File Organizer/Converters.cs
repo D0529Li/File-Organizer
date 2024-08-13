ec0a4c440afea91b4def2a6c26879ee8a2c52c40
David Li <liyuchen990529@gmail.com>
2024-08-13 14:26:17 -0400

    Add NullImageConverter

--- File Organizer/Converters.cs
+++ File Organizer/Converters.cs
@@ -48,3 +48,24 @@ public sealed class InverseBooleanConverter : IValueConverter
         return null;
     }
 }
+
+public class NullImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null || (value is string val && val == string.Empty))
+            return DependencyProperty.UnsetValue;
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // According to https://msdn.microsoft.com/en-us/library/system.windows.data.ivalueconverter.convertback(v=vs.110).aspx#Anchor_1
+        // (kudos Scott Chamberlain), if you do not support a conversion 
+        // back you should return a Binding.DoNothing or a 
+        // DependencyProperty.UnsetValue
+        return Binding.DoNothing;
+        // Original code:
+        // throw new NotImplementedException();
+    }
+}
